325e27ea3808ab06be56b55293569cde34fcc298
Johnson <johnson732j@gmail.com>
2026-05-09 10:51:48 +0530

    first commit

---
+++ src/main/java/com/pesudadi/pesudadi/PesuDaDiApplication.java
@@ -0,0 +1,13 @@
+package com.pesudadi.pesudadi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PesuDaDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PesuDaDiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pesudadi/pesudadi/PesuDaDiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pesudadi.pesudadi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PesuDaDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
